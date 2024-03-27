610f47ace80f4bda0f2dbf4a1ce1861fce880d9f
chen <chensiting@163.sufe.edu.cn>
2024-03-27 17:37:58 +0800

    update some data

--- online_QA_test.py
+++ online_QA_test.py
@@ -6,8 +6,16 @@ import logging
 from openai import OpenAI
 from dotenv import load_dotenv
 
+logging.basicConfig(level=logging.INFO)
 os.environ["http_proxy"] = "http://localhost:33210"
 os.environ["https_proxy"] = "http://localhost:33210"   # 开梯子时要加上，33210是我电脑上梯子的端口
+load_dotenv()
+api_key = 'sk-60kV9nDoV5LSZfeq24Fc7f63F1Bc4216A091E7Fb45EeA604'
+base_url = 'https://api.chatgptid.net/v1'
+kwargs = {"temperature": 0.3}
+num_round = 3
+batch_size = 10
+start_row = 0
 
 class OpenAILLM():
     def __init__(self, api_key, base_url, kwargs):
@@ -24,7 +32,7 @@ class OpenAILLM():
         result = response.choices[0].message.content
         return result
 
-def generate_conv(gpt, prompt_template, row, num_round):
+def generate_conv(gpt, Q_prompt_template,A_prompt_template, row, num_round):
     '''
     为一行row生成num_rounds轮对话
     '''
@@ -32,9 +40,10 @@ def generate_conv(gpt, prompt_template, row, num_round):
     convs = []
     for i in range(1,num_round+1):
         input = json.dumps({"post": row['post'], "history": history},ensure_ascii=False)
-        prompt = prompt_template.replace('<input>', input)
-        question = gpt.generate(prompt)
-        answer = gpt.generate('请你以财经专家或统计专家的身份回答问题：'+question)
+        Q_prompt = Q_prompt_template.replace('<input>', input)
+        question = gpt.generate(Q_prompt)
+        A_prompt = A_prompt_template.replace('<question>', question)
+        answer = gpt.generate(A_prompt)
         conv_json = {
             "instruction": question,
             "output": answer,
@@ -45,31 +54,21 @@ def generate_conv(gpt, prompt_template, row, num_round):
     return convs  
 
 def main():
-    load_dotenv()
-    api_key = os.environ.get('api_key')
-    base_url = os.environ.get('base_url')
-    kwargs = {
-        "temperature": 0.3
-    }
-    gpt = OpenAILLM(api_key,base_url,kwargs)
-    num_round = 3
-    batch_size = 10
-    start_row = 0
-    
+    gpt = OpenAILLM(api_key,base_url,kwargs) 
     with open("prompts\\gen_Q_template.txt", 'r', encoding='utf-8') as prompt_file:
-        prompt_template = prompt_file.read()
-    
+        Q_prompt_template = prompt_file.read()
+    with open("prompts\\gen_A_template.txt", 'r', encoding='utf-8') as prompt_file:
+        A_prompt_template = prompt_file.read()
+
     df_ = pd.read_csv(r'Data\filtered_questions.csv', encoding='utf-8')  
     result = []
-    for index, row in df_.iloc[start_row:].iterrows():  # 从选定的行开始迭代
+    for index, row in df_.iloc[start_row:].iterrows():
         logging.info(f'Processing {index}...')
-        convs = generate_conv(gpt, prompt_template, row, num_round)
+        convs = generate_conv(gpt, Q_prompt_template,A_prompt_template, row, num_round)
         result.extend(convs)
         
         if (index - start_row + 1) % batch_size == 0:
-            start_index = index + 1 - batch_size
-            end_index = index
-            with open(f'Data\\from_{start_index}_to_{end_index}.json', 'w', encoding='utf-8') as file:
+            with open(f'Data\\from_{index + 1 - batch_size}_to_{index}.json', 'w', encoding='utf-8') as file:
                 json.dump(result, file, indent=4, ensure_ascii=False)
             result = []
         
@@ -79,14 +78,23 @@ if __name__ == "__main__":
 """
 生成逻辑：逐帖子产生对话，即产生一个帖子的n轮，再产生下一个帖子的n轮，以此类推，每完成m个帖子的n轮对话，就保存一次
 """
-# %%
-# # 计算'Data\\from_0_to_3.json'中output的平均长度
-# import json
-# import pandas as pd
-# with open('Data\\from_0_to_20.json', 'r', encoding='utf-8') as file:
-#     data = json.load(file)
 
-# output_lengths = [len(item['output']) for item in data]
-# average_length = sum(output_lengths) / len(output_lengths)
-# print(f"Average output length: {average_length}")
 # %%
+import json
+result = []
+for file_name in os.listdir('Data'):
+    if file_name.startswith('from_') and file_name.endswith('.json'):
+        with open(f'Data/{file_name}', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            result.extend(data)
+
+with open('Data\\online_QA.json', 'w', encoding='utf-8') as file:
+    json.dump(result, file, indent=4, ensure_ascii=False)
+    
+# 计算output的平均长度
+with open('Data\\online_QA.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+output_lengths = [len(item['output']) for item in data]
+average_length = sum(output_lengths) / len(output_lengths)
+print(f"Average output length: {average_length}")
